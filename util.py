aedc46897cf4072f786c3f2a4016efe9d0c6d971
purvisindhani17 <purvisindhani17@gmail.com>
2025-09-22 00:19:23 +0530

    a

--- util.py
+++ util.py
@@ -8,7 +8,7 @@ __model =None
 
 def get_estimated_price(location,sqft,bhk,bath):
     try:
-        loc_index = __data_columns.index(location.lower())
+        loc_index = __data_columns.index(location.strip.lower())
     except:
         loc_index = -1
     x = np.zeros(len(__data_columns))
@@ -24,12 +24,12 @@ def load_saved_artifacts():
     print("Loading saved artifacts")
     global __locations
     global __data_columns
-    with open("./artifacts/columns.json",'r') as f:
+    with open("./artifcats/columns.json",'r') as f:
         __data_columns=json.load(f)['data_columns']
         __locations=__data_columns[3:]
     global __model
     if __model is None:
-        with open("./artifacts/bangalore_home_prices_model.pickle",'rb') as f:
+        with open("./artifcats/bangalore_home_prices_model.pickle",'rb') as f:
          __model=pickle.load(f)
     print("Loaded saved artifacts ..done")
 if __name__ == '__main__':
